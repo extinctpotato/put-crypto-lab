3eaf3fdcb08b9aace7431f566a5aa87bc82a677a
Adam Olech <nddr89@gmail.com>
2021-11-07 08:10:43 +0100

    Add bbs generator stub

---
+++ bbs_generator.py
@@ -0,0 +1,35 @@
+from math import gcd
+from random import randint
+
+def is_congruent(a, b, n):
+    return a%n == b%n
+
+def valid_for_blum(a):
+    return is_congruent(a, 3, 4)
+
+def pick_random_a(n):
+    a = n
+    while gcd(a, n) != 1:
+        a = randint(n, n*2)
+
+    return a
+
+def bbs(n, r, a):
+    x = []
+
+def bbs_preset():
+    p = 431
+    q = 347
+    n = p*q
+    a = pick_random_a(n)
+    r = 20
+
+    print(f'n: {n}, a: {a}')
+
+    assert valid_for_blum(p)
+    assert valid_for_blum(q)
+
+    return bbs(n, r, a)
+
+if __name__ == '__main__':
+    bbs_preset()
